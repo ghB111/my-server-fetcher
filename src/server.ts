328d88a3e74c784af3451e3048e423f7149b182f
Roman <b111forjob@gmail.com>
2021-08-01 14:52:00 +0700

    Create server.ts

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import { FetchingButtonsBot } from "tg-bot-button-data-fetching";
+import * as fs from 'fs';
+
+import { execSync } from 'child_process';
+
+const token: string = fs.readFileSync('./res/token.secret', 'utf8').split('\n')[0];
+const bot = new FetchingButtonsBot(token, {polling: true});
+
+bot.addButton('Temp', () => {
+    let stdout: string = execSync("sensors | grep temp1 | awk '{print $2}'").toString();
+    return stdout;
+})
